686c0f1be3cbfa1278b857635f4da3b9c88eb19b
Thomas Obst <obst@hlrs.de>
2025-01-31 21:05:40 +0100

    adjust motor parameters and verbosity

--- src/main.cpp
+++ src/main.cpp
@@ -25,14 +25,14 @@ U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
 // Motor conversion from erpm to speed and vice versa
 double speed(double erpm) {
-    float number_poles = 14.0;
+    float number_poles = 50.0;
     float wheel_diameter = 0.622; // 622 mm
     float wheel_circumference = wheel_diameter * 3.14159;
     return erpm /  number_poles * wheel_circumference / 60;
 }
 
 double new_rpm(double speed) {
-    float number_poles = 14.0;
+    float number_poles = 50.0;
     float wheel_diameter = 0.622; // 622 mm
     float wheel_circumference = wheel_diameter * 3.14159;
     return speed * 60 / wheel_circumference * number_poles;
@@ -44,8 +44,6 @@ struct BikeForces {
     double slopeForce;
     double totalForce;
 };
-
-
 
 BikeForces calculateForces(double v, double m, double CdA, double Crr, double slope) {
     BikeForces forces;
@@ -100,10 +98,10 @@ unsigned long currentTime = millis();
 unsigned long newTime = millis();
 unsigned long lastTime = millis();
 unsigned long deltaT = 0;
-double max_rpm = 10000.0;
+double max_rpm = 20000.0;
 double threshold_rpm = 500;
 bool rpm_control = false;
-bool verbosity = false;
+bool verbose = true;
 
 void loop() {
   i=i+1;
@@ -124,7 +122,7 @@ void loop() {
   /** Call the function getVescValues() to acquire data from VESC   */
   if ( UART.getVescValues() && deltaT > 0) 
   {
-    if (verbosity) 
+    if (verbose) 
     {
       Serial.print("\rRPM: ");
       Serial.print(UART.data.rpm);
@@ -150,42 +148,54 @@ void loop() {
         velocity = speed(UART.data.rpm);
 
         BikeForces forces = calculateForces(velocity, mass, CdA, Crr, slope);
-        if (verbosity)
+        if (verbose)
         {
-          Serial.print("\r Luftwiderstand: ");
+          Serial.print("Speed: ");
+          Serial.print(velocity);
+          /*Serial.print("Luft: ");
           Serial.print(forces.airResistance);
           Serial.print(" N, ");
-          Serial.print("Rollwiderstand: ");
+          Serial.print("Roll: ");
           Serial.print(forces.rollingResistance);
           Serial.print(" N, ");
-          Serial.print("Steigungswiderstand: ");
+          Serial.print("Steigung: ");
           Serial.print(forces.slopeForce);
-          Serial.print(" N, ");
+          Serial.print(" N, ");*/
           Serial.print("Gesamtkraft: ");
           Serial.print(forces.totalForce);
           Serial.print(" N");
         }
-        double propulsive_force = UART.data.avgInputCurrent * UART.data.inpVoltage / velocity;
+        double propulsive_force = - UART.data.avgInputCurrent * UART.data.inpVoltage / velocity;
+
         double acceleration = (propulsive_force - forces.totalForce) / mass;
 
         double new_velocity =  velocity + acceleration * deltaT/1000;
         double new_rpm_value = new_rpm(new_velocity);
-
+        if (verbose)
+        { 
+          Serial.print("propulsF: ");
+          Serial.print(propulsive_force);
+          Serial.print("Acc: ");
+          Serial.print(acceleration);
+        }
         double delta_rpms = new_rpm_value - UART.data.rpm;
 
         if (new_rpm_value > max_rpm) 
         {
           new_rpm_value = max_rpm;
         }
-        if (verbosity)
+        if (verbose)
         {
-        u8g2.drawStr(0,20,("RPM" + String(UART.data.rpm)).c_str());
-        u8g2.drawStr(0,30,("deltaRPM" + String(delta_rpms)).c_str());
-        u8g2.drawStr(0,10,("Input Volt." + String(UART.data.inpVoltage)).c_str());
-        u8g2.drawStr(0,40,("target rpm " + String(new_rpm_value)).c_str());
-        u8g2.drawStr(0,50,("Inp. Curr." + String(UART.data.avgInputCurrent)).c_str());
+          Serial.print("newRPM: ");
+          Serial.print(new_rpm_value);
+
+          u8g2.drawStr(0,20,("RPM" + String(UART.data.rpm)).c_str());
+          u8g2.drawStr(0,30,("deltaRPM" + String(delta_rpms)).c_str());
+          u8g2.drawStr(0,10,("Input Volt." + String(UART.data.inpVoltage)).c_str());
+          u8g2.drawStr(0,40,("target rpm " + String(new_rpm_value)).c_str());
+          u8g2.drawStr(0,50,("Inp. Curr." + String(UART.data.avgInputCurrent)).c_str());
         }
-        // UART.setRPM(new_rpm_value);
+        UART.setRPM(new_rpm_value);
       }
     } 
     else 
